39a5c339010357220ac72b9e2cfb0d924d73a531
Gvin <gvin.sim@gmail.com>
2018-12-22 12:34:14 +0300

    [#6] Save Enabled\Disabled state
    Added saving enabled-disabled state on enabling-disabling.
    Added automatic start of correction if stored state is enabled.

--- Source/VolumeCorrector/Properties/Settings.Designer.cs
+++ Source/VolumeCorrector/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace VolumeCorrector.Properties {
                 this["LanguageCode"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Enabled {
+            get {
+                return ((bool)(this["Enabled"]));
+            }
+            set {
+                this["Enabled"] = value;
+            }
+        }
     }
 }
